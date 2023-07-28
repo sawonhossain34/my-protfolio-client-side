3cff1f7b6d26e68c2563349666258c3d3a35c401
sawonhossain34 <sawonhossain3491@gmail.com>
2023-07-29 00:01:23 +0600

    required set contact

--- src/Home/Contact/Contact.jsx
+++ src/Home/Contact/Contact.jsx
@@ -20,11 +20,11 @@ const Contact = () => {
             <h3 className='text-center text-4xl font-bold text-amber-600 my-10 pt-5'>Contact Me</h3>
             <form ref={form} onSubmit={sendEmail} className='p-10 text-amber-600 md:w-full lg:w-1/2 mx-auto border-2 border-amber-600 rounded m-5 '>
                 <label className='text-xl'>Name</label>
-                <input type="text" className='input-design' name="from_name" placeholder='Enter your name' />
+                <input type="text" className='input-design' name="from_name" placeholder='Enter your name' required />
                 <label className='text-xl'>Email</label>
-                <input type="email" className='input-design required' name="form_email" placeholder='Enter your email' />
+                <input type="email" className='input-design required' name="form_email" placeholder='Enter your email' required />
                 <label className='text-xl'>Message</label>
-                <textarea name="message" className='input-design required' placeholder='Enter your message' />
+                <textarea name="message" className='input-design required' placeholder='Enter your message' required/>
                 <input  className='text-xl btn bg-amber-600 hover:text-amber-600 text-white' type="submit" value="Send" />
             </form>
         </div>
