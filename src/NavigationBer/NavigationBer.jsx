7a1b38129b9173e8ff62c03d2b7298b6662f43ec
sawonhossain34 <sawonhossain3491@gmail.com>
2023-09-17 01:15:11 +0600

    navigation w-full and out of aria

--- src/NavigationBer/NavigationBer.jsx
+++ src/NavigationBer/NavigationBer.jsx
@@ -3,30 +3,31 @@ const NavigationBer = () => {
 
     const listItem =
         <>
-            <a href="#"  className="mr-2 font-bold text-amber-600 text-xl">Home</a>
-            <a href="#project"  className="mr-2 font-bold text-amber-600 text-xl">Project</a>
-            <a href="#about"  className="mr-2 font-bold text-amber-600 text-xl">About</a>
-            <a href="#contact"  className="mr-2 font-bold text-amber-600 text-xl">Contact</a>
+            <a href="#" className="mr-2 font-bold text-amber-600 text-xl">Home</a>
+            <a href="#project" className="mr-2 font-bold text-amber-600 text-xl">Project</a>
+            <a href="#about" className="mr-2 font-bold text-amber-600 text-xl">About</a>
+            <a href="#contact" className="mr-2 font-bold text-amber-600 text-xl">Contact</a>
         </>
     return (
-        <div className="navbar max-w-screen-xl mx-auto z-50 fixed top-0 bg-black">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost text-amber-700 lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            // <div className="flex justify-between py-5 w-[1260px] z-50 fixed top-0 bg-black">
+            <div className="flex justify-between py-5 px-5 lg:px-10 md:px-8 sm:px-5 w-full max-w-screen-xl mx-auto z-50 fixed top-0 bg-black">
+                <div className=" ml-10">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost text-amber-700 lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {listItem}
+                        </ul>
+                    </div>
+                    <a className="text-amber-600 normal-case text-4xl font-bold">Sawon Protfolio</a>
+                </div>
+                <div className=" hidden lg:flex mr-8 ">
+                    <ul className="menu menu-horizontal px-1">
                         {listItem}
                     </ul>
                 </div>
-                <a className="text-amber-600 normal-case text-4xl font-bold">Sawon Protfolio</a>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {listItem}
-                </ul>
-            </div>
-        </div>
     );
 };
 
